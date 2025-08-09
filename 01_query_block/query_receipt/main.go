53a69a3fcc15faf8d13cff1dbf1c803456358f5c
yangneng <897307454@qq.com>
2025-08-09 10:45:22 +0800

    查看收据

---
+++ 01_query_block/query_receipt/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/ethereum/go-ethereum/rpc"
+	"github.com/joho/godotenv"
+	"log"
+	"math/big"
+	"os"
+)
+
+// 查看收据
+func main() {
+	envErr := godotenv.Load()
+	if envErr != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	apiKey := os.Getenv("infura_API_KEY")
+	client, err := ethclient.Dial("https://sepolia.infura.io/v3/" + apiKey)
+	if err != nil {
+		log.Fatal(err)
+	}
+	blockNumber := big.NewInt(5671744)
+	blockHash := common.HexToHash("0xae713dea1419ac72b928ebe6ba9915cd4fc1ef125a606f90f5e783c47cb1a4b5")
+	receiptByHash, err := client.BlockReceipts(context.Background(), rpc.BlockNumberOrHashWithHash(blockHash, false))
+	if err != nil {
+		log.Fatal(err)
+	}
+	receiptByNum, err := client.BlockReceipts(context.Background(), rpc.BlockNumberOrHashWithNumber(rpc.BlockNumber(blockNumber.Int64())))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(receiptByHash[0] == receiptByNum[0])
+	for _, receipt := range receiptByHash {
+		fmt.Println(receipt.Status)
+		fmt.Println(receipt.Logs)
+		fmt.Println(receipt.TxHash.Hex())
+		fmt.Println(receipt.TransactionIndex)
+		fmt.Println(receipt.ContractAddress.Hex())
+		break
+	}
+
+	txHash := common.HexToHash("0x20294a03e8766e9aeab58327fc4112756017c6c28f6f99c7722f4a29075601c5")
+	receipt, err := client.TransactionReceipt(context.Background(), txHash)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(receipt.Status)
+	fmt.Println(receipt.Logs)
+	fmt.Println(receipt.TxHash.Hex())
+	fmt.Println(receipt.TransactionIndex)
+	fmt.Println(receipt.ContractAddress)
+}
