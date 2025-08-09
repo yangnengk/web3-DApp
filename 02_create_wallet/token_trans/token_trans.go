5356e267ce8a50190a691cbe0f0dc9a418f84805
yangneng <897307454@qq.com>
2025-08-09 15:23:14 +0800

    代币 转账

--- 02_create_wallet/token_trans/token_trans.go
+++ 02_create_wallet/token_trans/token_trans.go
@@ -4,17 +4,20 @@ import (
 	"context"
 	"crypto/ecdsa"
 	"fmt"
+	"github.com/ethereum/go-ethereum"
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/ethclient"
 	"github.com/joho/godotenv"
+	"golang.org/x/crypto/sha3"
 	"log"
 	"math/big"
 	"os"
 )
 
-// 发送一个eth
+// 代币转账
 func main() {
 	envErr := godotenv.Load()
 	if envErr != nil {
@@ -23,6 +26,7 @@ func main() {
 	apiKey := os.Getenv("INFURA_API_KEY")
 	PRIVATE_KEY := os.Getenv("PRIVATE_KEY")
 	client, err := ethclient.Dial("https://sepolia.infura.io/v3/" + apiKey)
+
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -40,24 +44,46 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	value := big.NewInt(100000) // in wei (1 eth = 10**18)
-	var gasLimit = uint64(21000)
+	value := big.NewInt(0)
 	gasPrice, err := client.SuggestGasPrice(context.Background())
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	toAddress := common.HexToAddress("0xf5f0A5135486fF2715b1dfAead54eEaFfe6B8404")
+	tokenAddress := common.HexToAddress("0xfadea654ea83c00e5003d2ea15c59830b65471c0")
+
+	transferFnSignature := []byte("transfer(address,uint256)")
+	hash := sha3.NewLegacyKeccak256()
+	hash.Write(transferFnSignature)
+	methodID := hash.Sum(nil)[:4]         //取前四个字节
+	fmt.Println(hexutil.Encode(methodID)) // 0xa9059cbb
+	paddedAddress := common.LeftPadBytes(toAddress.Bytes(), 32)
+	fmt.Println(hexutil.Encode(paddedAddress))
+	amount := new(big.Int)
+	amount.SetString("10000000000000000000", 10) // 设置代币转账数量为1000个代币（假设代币精度为18位小数）
+	paddedAmount := common.LeftPadBytes(amount.Bytes(), 32)
+	fmt.Println(hexutil.Encode(paddedAmount))
 	var data []byte
+	data = append(data, methodID...)
+	data = append(data, paddedAddress...)
+	data = append(data, paddedAmount...)
+
+	gasLimit, err := client.EstimateGas(context.Background(), ethereum.CallMsg{
+		To:   &toAddress,
+		Data: data,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("gasLimit =", gasLimit)
 	tx := types.NewTx(&types.LegacyTx{
 		Nonce:    nonce,
-		To:       &toAddress,
+		To:       &tokenAddress, // 代币合约地址
 		Value:    value,
 		Gas:      gasLimit,
 		GasPrice: gasPrice,
 		Data:     data,
 	})
-
 	chainID, err := client.NetworkID(context.Background())
 	if err != nil {
 		log.Fatal(err)
